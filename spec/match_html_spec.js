24b5a897baa7342e57ef096575c866361e966588
Rafael Garcia <rafbgarcia@gmail.com>
2017-01-25 19:18:26 -0300

    Add another failing scenario

--- spec/match_html_spec.js
+++ spec/match_html_spec.js
@@ -1,7 +1,15 @@
 var matchHTML = require('../index');
 
-it("should respond with hello world", function() {
-  const given = '<!DOCTYPE html>'
+it("doesn't care for blank lines", function() {
   const expected = "<!DOCTYPE html>\n"
+  const given = '<!DOCTYPE html>'
+
+  expect(matchHTML(expected, given)).toEqual(true);
+});
+
+it("knows how to handle comments", function() {
+  const expected = "<!DOCTYPE html>\n<!-- Isso é um comentário -->"
+  const given = '<!DOCTYPE html>'
+
   expect(matchHTML(expected, given)).toEqual(true);
 });
